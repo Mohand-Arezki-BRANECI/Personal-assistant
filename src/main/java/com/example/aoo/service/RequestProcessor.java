77160598f9cd6a0b7871e3f3f22e29d1ec96c260
Aurélie <Aurlie>
2023-06-19 16:44:06 +0200

    create DateTimeService + archi

---
+++ src/main/java/com/example/aoo/service/RequestProcessor.java
@@ -0,0 +1,30 @@
+package com.example.aoo.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+@Service
+public class RequestProcessor {
+    private final DateTimeService timeService;
+
+    public RequestProcessor(DateTimeService timeService) {
+        this.timeService = timeService;
+    }
+
+    public ResponseEntity processRequest(String request) {
+        request = request.toLowerCase();
+        String [] requestSplit= request.split(" ");
+
+        if(requestSplit[0].equals("!heure")){
+            return DateTimeService.getTime();
+        }
+        if(requestSplit[0].equals("!date")){
+            return DateTimeService.getDate();
+        }
+
+        return new  ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+    }
+}
+
