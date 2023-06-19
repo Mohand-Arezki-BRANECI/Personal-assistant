9ec668d10e2cc12b81be37af52d47697c2b31a0f
Kenz Touari <kenztouari@icloud.com>
2023-06-19 16:04:20 +0200

    googleapi

--- src/main/java/com/example/aoo/AooApplication.java
+++ src/main/java/com/example/aoo/AooApplication.java
@@ -2,9 +2,16 @@ package com.example.aoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AooApplication {
+    @GetMapping
+    public String welcom(){
+        return "welcome to google";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AooApplication.class, args);
