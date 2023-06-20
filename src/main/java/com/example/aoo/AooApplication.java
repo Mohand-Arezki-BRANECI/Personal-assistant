e514cd5cd45fdbbeba46da3b72d5820d924f6946
Kenz Touari <kenztouari@icloud.com>
2023-06-20 14:50:57 +0200

    calendarok

--- src/main/java/com/example/aoo/AooApplication.java
+++ src/main/java/com/example/aoo/AooApplication.java
@@ -2,16 +2,12 @@ package com.example.aoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@RestController
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AooApplication {
-    @GetMapping
-    public String welcom(){
-        return "welcome to google";
-    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(AooApplication.class, args);
